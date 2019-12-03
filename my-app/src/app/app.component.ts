7fbce90c9d998caecd91d4cdceeba14b1eaec850
zalmek <zalmek23@gmail.com>
2019-12-03 11:42:41 +0300

    get set

--- my-app/src/app/app.component.ts
+++ my-app/src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name= 'place'
-  title = 'my-app';
+  age : number = 23
+  name:string= 'place'
+  title = 'my-app'
 }
   
--- my-app/src/app/child/child.component.ts
+++ my-app/src/app/child/child.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-child',
@@ -7,6 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ChildComponent implements OnInit {
   name='good';
+  @Input() userName:string;
+  _userAge:number;
+  @Input()
+  set userAge(age:number){
+    if(age<0){
+      this._userAge = 0;
+    }
+    else if (age>100){
+      this,this._userAge=100
+    }
+    else {
+      this._userAge=age
+    }
+  }
+  get userAge(){
+    return this._userAge
+  }
   constructor() { }
   ngOnInit() {
   }
